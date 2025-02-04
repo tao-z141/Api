036092440e7b56e299122934911859cfe0d003ac
tao-z141 <taozaddi@gmail.com>
2025-02-04 14:57:08 +0100

    Update album.mjs

--- src/models/album.mjs
+++ src/models/album.mjs
@@ -1,1 +1,21 @@
+import mongoose from 'mongoose';
 
+const AlbumSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String },
+  photos: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Photo' }],
+  created_at: { type: Date, default: Date.now }
+}, {
+  collection: 'albums',
+  minimize: false,
+  versionKey: false
+}).set('toJSON', {
+  transform: (doc, ret) => {
+    const retUpdated = ret;
+    retUpdated.id = ret._id;
+    delete retUpdated._id;
+    return retUpdated;
+  }
+});
+
+export default mongoose.model('Album', AlbumSchema);
